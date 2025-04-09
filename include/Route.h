c2adbcb6fd5f629ef231951f5dbff2acef0223a3
monnica_88 <72393725+monnicam88@users.noreply.github.com>
2025-04-09 12:59:43 +0300

    Add Doxygen documentation for findRoutesRecursive()

--- include/Route.h
+++ include/Route.h
@@ -23,6 +23,26 @@ public:
     );
 
 private:
+/**
+ * @brief Recursively finds all possible routes between two cities.
+ *
+ * This function performs a recursive search through the list of flights to find
+ * all valid routes from the current city to the destination city. It takes into 
+ * account the maximum number of allowed flights, if specified.
+ * 
+ * @param flights List of available flights.
+ * @param currentCity The city from which the current search begins.
+ * @param destination The target city to reach.
+ * @param visited List of already visited cities to avoid cycles.
+ * @param currentPath The current path being explored.
+ * @param currentPrice Accumulated price of the current route.
+ * @param routes Collection to store all valid found routes.
+ * @param maxFlights Maximum number of allowed flights: 
+ *                   - When it is 0 means maxFlights option is not used in the request
+ *                   - When it is not 0 means maxFlights option is used in the request 
+ *                     with certain value.
+ * @param flightsTaken Number of flights taken so far in the current path.
+ */
     static void findRoutesRecursive(
         const std::vector<Flight>& flights,
         const std::string& currentCity,
